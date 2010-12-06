84d417fcc24e2ba51e6ebb315d0e228ca69c4a10
Markus Radmayr <markus.radmayr@gmail.com>
2010-12-06 21:26:36 +0100

    Logger in Main.java + output in Main() method

--- src/at/jku/ce/Main.java
+++ src/at/jku/ce/Main.java
@@ -1,11 +1,30 @@
 package at.jku.ce;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Set<DomainObject> objectSet = new HashSet<DomainObject>();
+
+		objectSet.add(new DomainObject("Helga"));
+		objectSet.add(new DomainObject("Walter"));
+		objectSet.add(new DomainObject("Hannah"));
+		objectSet.add(new DomainObject("Paul"));
+		objectSet.add(new DomainObject("Claudia"));
+
+		for (DomainObject domainObject : objectSet) {
+		  LOGGER.debug(domainObject.id);
+		}
 	}
 
 }
